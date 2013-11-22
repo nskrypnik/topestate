8ff5112b45d65fc985c1faab956dd630a60b91f9
Niko Skrypnik <nskrypnik@gmail.com>
2013-11-22 18:36:46 +0200

    add some crossbrowser capability

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 
+
+// Pseudoclasses fix for ie8
+
+$('a').mousedown(function(){
+	$(this).addClass('active');
+});
+
+$('a').mouseup(function(){
+	$(this).removeClass('active');
+});
+
+$('label').click(function(){
+	input_id = $(this).attr('for');
+	input = $('#' + input_id);
+	if (input.attr('type') == 'radio'){
+		radio_group = input.attr('name');
+		$('input[name=' + radio_group + ']').attr('checked', false);
+		input.attr('checked', true);
+
+	}
+	if (input.attr('type') == 'checkbox'){
+		$(this).toggleClass('checked');
+	}
+});
